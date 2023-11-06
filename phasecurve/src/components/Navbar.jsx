cbfc6717e84013333997b5714e6ff74212b304fe
oti-favour <favourgraceoti@gmail.com>
2023-11-06 12:09:56 +0100

    navbar

---
+++ phasecurve/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/">PhaseCurve</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+     );
+};
+ 
+export default Navbar;
